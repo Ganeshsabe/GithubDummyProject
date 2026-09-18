762e5ddd4b0e41ee7492fc29b1a585fae2248f7d
Ganeshsabe <ganeshsabe5@gmail.com>
2026-09-18 11:44:32 +0530

    Initial commit

---
+++ src/main/java/com/example/GithubDummyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubDummyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubDummyProjectApplication.class, args);
+	}
+
+}
